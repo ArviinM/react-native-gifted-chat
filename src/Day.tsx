0be6c9be8808ce49db9576ffc9e8e4444c7af6bc
arvin-rhen <arvin.medina@thundr.ph>
2024-04-28 21:04:39 +0800

    thundr: changes to day and added today, yesterday, and day label for old messages functionality

--- src/Day.tsx
+++ src/Day.tsx
@@ -9,14 +9,15 @@ import {
   TextStyle,
   TextProps,
 } from 'react-native'
-import dayjs from 'dayjs'
+// import dayjs from 'dayjs'
 
 import Color from './Color'
 import { StylePropType, isSameDay } from './utils'
-import { DATE_FORMAT } from './Constant'
+// import { DATE_FORMAT } from './Constant'
 import { IMessage } from './Models'
 
-import { useChatContext } from './GiftedChatContext'
+// import { useChatContext } from './GiftedChatContext'
+import moment from 'moment'
 
 const styles = StyleSheet.create({
   container: {
@@ -46,30 +47,39 @@ export interface DayProps<TMessage extends IMessage = IMessage> {
 }
 
 export function Day<TMessage extends IMessage = IMessage>({
-  dateFormat = DATE_FORMAT,
+  // dateFormat = DATE_FORMAT,
   currentMessage,
   previousMessage,
   containerStyle,
   wrapperStyle,
   textStyle,
 }: DayProps<TMessage>) {
-  const { getLocale } = useChatContext()
+  // const { getLocale } = useChatContext()
 
   if (currentMessage == null || isSameDay(currentMessage, previousMessage)) {
     return null
   }
 
+  const messageDate = moment(currentMessage.createdAt);
+  const today = moment();
+  const yesterday = moment().subtract(1, 'day');
+
+  let dayLabel;
+  if (messageDate.isSame(today, 'day')) {
+    dayLabel = 'Today';
+  } else if (messageDate.isSame(yesterday, 'day')) {
+    dayLabel = 'Yesterday';
+  } else {
+    dayLabel = messageDate.format('MMMM DD, dddd');
+  }
+
   return (
     <View style={[styles.container, containerStyle]}>
       <View style={wrapperStyle}>
-        <Text style={[styles.text, textStyle]}>
-          {dayjs(currentMessage.createdAt)
-            .locale(getLocale())
-            .format(dateFormat)}
-        </Text>
+        <Text style={[styles.text, textStyle]}>{dayLabel}</Text>
       </View>
     </View>
-  )
+  );
 }
 
 Day.propTypes = {
